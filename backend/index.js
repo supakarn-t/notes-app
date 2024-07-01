8b3cf023faef77ae43c0a9bd2613711855e4ce69
supakarn-t <hikaru8889@gmail.com>
2024-07-01 12:19:18 +0700

    setup

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI);
+
+const User = require("./models/user.model");
+const Note = require("./models/note.model");
+
+const express = require("express");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+const jwt = require("jsonwebtoken");
+const { authenticateToken } = require("./utilities");
+
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+
+// start server
+app.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT} 🌎`);
+});
+
+app.get("/", (req, res) => {
+	res.json({ data: "respond received from the server!" });
+});
+
+module.exports = app;
---
+++ backend/utilities.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+
+// middleware function
+function authenticateToken(req, res, next) {
+	// authorization header
+	// req.header -> authorization: Bearer <JWT token>
+	const authHeader = req.header["authorization"];
+
+	// check if authHeader exists, and save authHeader.split(" ")[1] into token
+	const token = authHeader && authHeader.split(" ")[1];
+
+	console.log(`access token: ${token}`);
+
+	if (!token) return res.sendStatus(401);
+
+	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+		if (err) return res.sendStatus(401);
+		console.log(user);
+		req.user = user;
+		next();
+	});
+}
+
+module.exports = { authenticateToken };
