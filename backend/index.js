4472c1ffb64ea5d27d75eff72da7e7aae95f46bf
supakarn-t <hikaru8889@gmail.com>
2024-07-01 13:37:23 +0700

    add model

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,10 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_URI);
 
+mongoose.connection.on("connected", () => {
+	console.log("Connected to MongoDB ✅");
+});
+
 const User = require("./models/user.model");
 const Note = require("./models/note.model");
 
---
+++ backend/models/note.model.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const noteSchema = new Schema({
+	title: { type: String, require: true },
+	content: { type: String, require: true },
+	tags: { type: [String], default: [] },
+	isPinned: { type: Boolean, require: false },
+	userId: { type: String, require: true },
+	createdOn: { type: Date, default: new Date().getTime() },
+});
+
+module.exports = mongoose.model("Note", noteSchema);
